97c15484a99e3a91ed941752ee2560ba64c26e0e
Danel668 <daneldanel668@mail.ru>
2023-05-25 17:00:33 +0300

    did 7 task

---
+++ DistanceFromAllVertexes/Algorithms/Johnson.cs
@@ -0,0 +1,89 @@
+﻿using DistanceFromGivenVertex.Algorithms;
+using GraphApp.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceFromAllVertexes.Algorithms
+{
+    public static class Johnson
+    {
+        public static int[][] AlgJohnson(List<List<int>> _matrix)
+        {
+            int[][] distances = new int[_matrix.Count][];
+            for (int i = 0; i < _matrix.Count; i++)
+            {
+                distances[i] = new int[_matrix.Count];
+            }
+
+            int[] h = new int[_matrix.Count + 1];
+
+            for (int i = 0; i < h.Length; i++) h[i] = int.MaxValue - 5000;
+            h[h.Length - 1] = 0;
+
+            // список ребер
+            var edges = new List<Tuple<int, int, int>>();
+
+            // матрицу смежности переводим в список ребер
+            for (int i = 0; i < _matrix.Count; i++)
+            {
+                for (int j = 0; j < _matrix.Count; j++)
+                {
+                    if (_matrix[i][j] != 0)
+                    {
+                        edges.Add(new Tuple<int, int, int>(i, j, _matrix[i][j]));
+                    }
+                }
+            }
+
+            for (int i = 0; i < _matrix.Count; i++)
+            {
+                edges.Add(new Tuple<int, int, int>(_matrix.Count, i, 0));
+            }
+
+            for (int i = 0; i < _matrix.Count; i++)
+            {
+                foreach (var e in edges)
+                {
+                    h[e.Item2] = Math.Min(h[e.Item2], h[e.Item1] + e.Item3);
+                }
+            }
+
+            foreach (var e in edges)
+            {
+                if (h[e.Item2] > h[e.Item1] + e.Item3)
+                {
+                    return new int[][] { };
+                }
+            }
+
+            for (int i = 0; i < _matrix.Count; i++)
+            {
+                for (int j = 0; j < _matrix.Count; j++)
+                {
+                    if (_matrix[i][j] != 0)
+                        _matrix[i][j] = _matrix[i][j] + h[j] - h[i];
+                }
+            }
+
+            // расчитываем расстояние по алгоритму Дейкстры
+            for (int i = 0; i < _matrix.Count; i++)
+            {
+                distances[i] = SearchDistancesFromGivenVertex.Dijkstra(i,_matrix);
+            }
+
+            for (int i = 0; i < _matrix.Count; i++)
+            {
+                for (int j = 0; j < _matrix.Count; j++)
+                {
+                    distances[i][j] = distances[i][j] + h[i] - h[j];
+                }
+            }
+
+            return distances;
+
+        }
+    }
+}
---
+++ DistanceFromAllVertexes/Program.cs
@@ -0,0 +1,57 @@
+﻿using DistanceFromAllVertexes.Algorithms;
+using GraphApp.IO;
+using GraphApp.Objects;
+using System.Diagnostics;
+using System.Text;
+
+namespace DistanceFromAllVertexes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var input = new InputFlagReader(args);
+                var output = new Output(args);
+
+                if (input.IsArgsContainsHeader())
+                {
+                    return;
+                }
+
+                var graph = new Graph(input.GetFactory());
+
+                var dst = Johnson.AlgJohnson(graph.AdjacencyMatrix());
+
+                if (dst.Length== 0)
+                {
+                    output.WriteLine("Graph contains a negative cycle.");
+                }
+                else
+                {
+                    var str = new StringBuilder();
+                    str.Append("Shortest paths lengths:\n");
+
+                    for (int i = 0; i < dst.Length; i++)
+                    {
+                        for (int j = 0; j < dst[i].Length; j++)
+                        {
+                            if (dst[i][j] != 0 && dst[i][j] < 100000 && dst[i][j] > -100000)
+                            {
+                                str.Append($"{i + 1} - {j + 1}: {dst[i][j]}\n");
+                            }
+                        }
+                    }
+                    output.WriteLine(str.ToString());
+                    str.Clear();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
