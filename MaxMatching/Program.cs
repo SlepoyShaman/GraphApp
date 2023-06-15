1b13b0f2278dea656e0276cbf2a85c983f47e54e
Danel668 <daneldanel668@mail.ru>
2023-06-15 16:33:38 +0300

    did ham

---
+++ MaxMatching/Program.cs
@@ -0,0 +1,39 @@
+﻿using GraphApp.IO;
+using GraphApp.Objects;
+
+namespace MaxMatching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var input = new InputFlagReader(args);
+                var output = new Output(args);
+
+                if (input.IsArgsContainsHeader())
+                {
+                    return;
+                }
+
+                var graph = new Graph(input.GetFactory());
+                int size = graph.AdjacencyMatrix().Count;
+
+                Algcuna alg = new Algcuna(graph.AdjacencyMatrix());
+                var maxMatching = alg.FindMaxMatching();
+
+                foreach (var edge in maxMatching)
+                {
+                    Console.WriteLine($"({edge.Item1 + 1}, {edge.Item2 + 1})");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
